88ce0e9a2d057137f3e59520c5836d283a836990
Gsandman8 <gage.e.sandman@gmail.com>
2023-08-22 15:38:36 -0700

    Added prompts when the button is pressed

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,14 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword() {
+  const passwordLength = prompt("How long would you like your password? (8-128 characters)");
+    if (passwordLength < 8 || passwordLength > 128){
+      alert("Please enter a valid password length.");
+      generatePassword();
+    }
+  const charTypes = prompt("What type(s) of characters would you like in your password? Your options are lowercase, uppercase, numbers and/or special characters.").split(",");
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

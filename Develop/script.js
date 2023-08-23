89ce69086751c5aae532d9765dc041952f4721d3
Gsandman8 <gage.e.sandman@gmail.com>
2023-08-22 18:20:17 -0700

    Prompts now require correct responses

--- Develop/script.js
+++ Develop/script.js
@@ -9,14 +9,37 @@ function writePassword() {
   passwordText.value = password;
 
 }
-function generatePassword() {
+
+function getPasswordLength() {
   const passwordLength = prompt("How long would you like your password? (8-128 characters)");
     if (passwordLength < 8 || passwordLength > 128){
       alert("Please enter a valid password length.");
-      generatePassword();
+      getPasswordLength();
     }
-  const charTypes = prompt("What type(s) of characters would you like in your password? Your options are lowercase, uppercase, numbers and/or special characters.").split(",");
+    else {
+      return passwordLength;
+    }
 }
+  
+  function getPasswordTypes() {
+  const charTypes = prompt("What type(s) of characters would you like in your password? Your options are lowercase, uppercase, numbers and/or special characters. (Please seperate your options with commas)").split(",");
+  for(let i=0; i<charTypes.length; i++){
+    charTypes[i].toLowerCase();
+  }
+    if(!charTypes.includes("lowercase") && !charTypes.includes("uppercase") && !charTypes.includes("numbers") && !charTypes.includes("special characters")) {
+      alert("Please enter valid character options.");
+      getPasswordTypes();
+      }
+    else{
+      return charTypes;
+    }
+  }
+
+  function generatePassword() {
+    getPasswordLength();
+    getPasswordTypes();
+    
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
